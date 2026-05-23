f4bd7ba139a6f7420cd90c2be50ef08295400194
mahalingam2188 <mahalingam.r@ekkomd.com>
2026-05-23 13:07:43 +0530

    Add S3 image upload replacing local disk storage

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,88 +1,54 @@
-import {promises as fs } from 'fs';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductRequest } from './dto/create-product.request';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { join } from 'path';
-import { PRODUCT_IMAGES } from './product-images';
 import { Prisma } from '@prisma/client';
 import { ProductsGateway } from './products.gateway';
-
-
-
-
-
-
+import { S3Service } from 'src/s3/s3.service';
 
 @Injectable()
 export class ProductsService {
 
-    constructor(private readonly prismaservice:PrismaService, private readonly ProductsGateway: ProductsGateway){} 
-    
+    constructor(
+        private readonly prismaservice: PrismaService,
+        private readonly ProductsGateway: ProductsGateway,
+        private readonly s3Service: S3Service,
+    ) {}
 
-    async createproducts(datas:CreateProductRequest, userid:number){
-
-        const product = await this.prismaservice.product.create(
-            {
-                data:{
-                    ...datas, userid,
-                }
-            }
-        )
+    async createproducts(datas: CreateProductRequest, userid: number) {
+        const product = await this.prismaservice.product.create({
+            data: { ...datas, userid },
+        });
         this.ProductsGateway.handleproductupdated();
-        return product
-        
-
+        return product;
     }
 
-   
-
-    async products(status?:string){
-        const args : Prisma.ProductFindManyArgs = {};
-        if(status == 'available')
-        {
-             args.where = {sold: false};
-             
+    async products(status?: string) {
+        const args: Prisma.ProductFindManyArgs = {};
+        if (status === 'available') {
+            args.where = { sold: false };
         }
         const products = await this.prismaservice.product.findMany(args);
         return Promise.all(
-            products.map(async(product)=> ({
+            products.map(async (product) => ({
                 ...product,
-                imageexists: await this.imageexists(product.id),
-            }))
-        )
-    }
-
-   private async imageexists(productid:number){
-    try{
-        await fs.access(
-               join(`${PRODUCT_IMAGES}/${productid}.jpg`),
-               fs.constants.F_OK,
+                imageexists: await this.s3Service.imageExists(`${product.id}.jpg`),
+            })),
         );
-        return true;
     }
-    catch(err){
-        return false;
+
+    async getproduct(productId: number) {
+        try {
+            return {
+                ...(await this.prismaservice.product.findUniqueOrThrow({ where: { id: productId } })),
+                imageexists: await this.s3Service.imageExists(`${productId}.jpg`),
+            };
+        } catch (err) {
+            throw new NotFoundException(`Product not found for id ${productId}`);
+        }
     }
-   }
 
-   async getproduct(productId:number){
-    try{ return {
-        ...(await this.prismaservice.product.findUniqueOrThrow({where : {id : productId}})), 
-    imageexists: await this.imageexists(productId)}; 
-   }
-   catch(err)
-   {
-    throw new NotFoundException(`the product is not found for the given ${productId}` );            
-   }
-}
-   
-async update(productId: number, data: Prisma.ProductUpdateInput) {
-    await this.prismaservice.product.update({
-      where: { id: productId },
-      data,
-    });
-    this.ProductsGateway.handleproductupdated();
-  }
-    
-
+    async update(productId: number, data: Prisma.ProductUpdateInput) {
+        await this.prismaservice.product.update({ where: { id: productId }, data });
+        this.ProductsGateway.handleproductupdated();
+    }
 }
---
+++ src/s3/s3.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { S3Client, PutObjectCommand, HeadObjectCommand } from '@aws-sdk/client-s3';
+
+@Injectable()
+export class S3Service {
+  private readonly s3: S3Client;
+  private readonly bucket: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.bucket = this.configService.getOrThrow('AWS_S3_BUCKET');
+    this.s3 = new S3Client({
+      region: this.configService.getOrThrow('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.getOrThrow('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.getOrThrow('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+  }
+
+  async uploadImage(key: string, buffer: Buffer, mimetype: string): Promise<string> {
+    await this.s3.send(
+      new PutObjectCommand({
+        Bucket: this.bucket,
+        Key: key,
+        Body: buffer,
+        ContentType: mimetype,
+      }),
+    );
+    return `https://${this.bucket}.s3.amazonaws.com/${key}`;
+  }
+
+  async imageExists(key: string): Promise<boolean> {
+    try {
+      await this.s3.send(new HeadObjectCommand({ Bucket: this.bucket, Key: key }));
+      return true;
+    } catch {
+      return false;
+    }
+  }
+}
